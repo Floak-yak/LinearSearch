1c8d9999745a363a2a84f7e3a897b7d17944d2b0
Khiem <aidolamgidi@gmail.com>
2024-12-26 08:52:40 +0700

    Search a number in an Array

---
+++ LinearSearch/SearchANumber/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] dataArray = { 1, 3, 5, 7, 9 }; // Tạo một mảng số nguyên để kiểm tra
+
+Algorithms alg = new Algorithms(); // Khởi tạo đối tượng của lớp Algorithms
+
+// Gọi hàm LinearSearch và in kết quả
+bool r1 = alg.LinearSearch(dataArray, 5); // Kết quả: True
+bool r2 = alg.LinearSearch(dataArray, 8); // Kết quả: False
+
+Console.WriteLine($"TargetValue: 5. Found: {r1}");
+Console.WriteLine($"TargetValue: 5. Found: {r2}");
+
+public class Algorithms
+{
+    public bool LinearSearch(int[] numbers, int targetValue)
+    {
+        foreach (int number in numbers)
+        {
+            if (number == targetValue)
+                return true;
+        }
+        return false;
+    }
+}
